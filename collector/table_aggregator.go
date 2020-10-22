b1afb61893b4053976d3a818686cdc5312a43cd4
Adrián del Campo <Adrian.Del.Campo.Hernandez@telefonica.com>
2020-10-22 18:17:15 +0200

    Applied patch from ricar repository to bring this collector back

---
+++ collector/table_aggregator.go
@@ -0,0 +1,127 @@
+// Copyright 2018 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector
+
+import (
+	"database/sql"
+	"regexp"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	// Regex the regular expression used for renaming the tables.
+	Regex = kingpin.Flag(
+		"aggregate_table_metrics.regex",
+		"Regex with capture groups for renaming the tables",
+	).Default("(.*)").String()
+
+	// Substitution The substitution expression used for renaming tables.
+	Substitution = kingpin.Flag(
+		"aggregate_table_metrics.substitution",
+		"Substitution string to apply to the table name",
+	).Default("$1").String()
+
+	defaultKeyProvider = func(stats TableStats) string {
+		return stats.schema + "." + stats.name
+	}
+)
+
+// TableStats A struct that contains the set of metrics for a schema.table
+type TableStats struct {
+	schema       string
+	name         string
+	commonLabels []string
+	metrics      map[string]float64
+	labels       map[string][]string
+}
+
+// MetricsDefinition A struct that contains the definition of a metric
+type MetricsDefinition struct {
+	name              string
+	metricType        prometheus.ValueType
+	metricDescription *prometheus.Desc
+}
+
+// TableAggregator Utility struct that aggregate metrics by table name
+type TableAggregator struct {
+	regex        string
+	substitution string
+	keyProvider  func(stats TableStats) string
+}
+
+// NewTableAggregator Constructor method that uses the default key provider (schema.table)
+func NewTableAggregator(regex string, substitution string) TableAggregator {
+	return TableAggregator{regex, substitution, defaultKeyProvider}
+}
+
+func (ta TableAggregator) processRow(f func(informationSchemaTableStatisticsRows *sql.Rows) (TableStats, error),
+	rows *sql.Rows, aggregatedStats map[string]TableStats) error {
+
+	tempStats, err := f(rows)
+
+	if err != nil {
+		return err
+	}
+
+	tableNameRegex := regexp.MustCompile(ta.regex)
+	tempStats.name = tableNameRegex.ReplaceAllString(tempStats.name, ta.substitution)
+
+	stats, found := aggregatedStats[ta.keyProvider(tempStats)]
+
+	if !found {
+		stats = TableStats{tempStats.schema, tempStats.name, tempStats.commonLabels, make(map[string]float64), make(map[string][]string)}
+	}
+
+	for k := range tempStats.metrics {
+		stats.metrics[k] += tempStats.metrics[k]
+
+		// Make sense to allow mixing labels? If different labels it is a different metric
+		stats.labels[k] = tempStats.labels[k]
+	}
+
+	aggregatedStats[ta.keyProvider(tempStats)] = stats
+
+	return nil
+}
+
+func (TableAggregator) sendMetrics(ch chan<- prometheus.Metric,
+	aggregatedStats map[string]TableStats, metrics []MetricsDefinition) error {
+	for _, table := range aggregatedStats {
+
+		for _, metric := range metrics {
+
+			if metricValue, ok := table.metrics[metric.name]; ok {
+
+				labels := []string{table.schema, table.name}
+
+				if len(table.commonLabels) > 0 {
+					labels = append(labels, table.commonLabels...)
+				}
+
+				if len(table.labels[metric.name]) > 0 {
+					labels = append(labels, table.labels[metric.name]...)
+				}
+
+				ch <- prometheus.MustNewConstMetric(
+					metric.metricDescription, metric.metricType, float64(metricValue),
+					labels...,
+				)
+			}
+		}
+	}
+
+	return nil
+}
---
+++ collector/table_aggregator_test.go
@@ -0,0 +1,221 @@
+// Copyright 2018 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	testMetricDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, performanceSchema, "testmetric"),
+		"The description of the test metric.",
+		[]string{"schema", "name", "zoperation"}, nil,
+	)
+)
+
+func TestSendMetrics(t *testing.T) {
+	ch := make(chan prometheus.Metric)
+	aggregator := TableAggregator{}
+	aggregatedStats := make(map[string]TableStats)
+	aggregatedStats["test.table"] = TableStats{
+		"test",
+		"table",
+		[]string{},
+		map[string]float64{
+			"readNormalCount":       float64(2),
+			"readSharedLocksCount":  float64(4),
+			"readHighPriorityCount": float64(5),
+		},
+		map[string][]string{
+			"readNormalCount":       []string{"label1"},
+			"readSharedLocksCount":  []string{"label2"},
+			"readHighPriorityCount": []string{"label3"},
+		},
+	}
+	metrics := [3]MetricsDefinition{
+		MetricsDefinition{"readNormalCount", prometheus.CounterValue, testMetricDesc},
+		MetricsDefinition{"readSharedLocksCount", prometheus.CounterValue, testMetricDesc},
+		MetricsDefinition{"readHighPriorityCount", prometheus.CounterValue, testMetricDesc},
+	}
+
+	go func() {
+		aggregator.sendMetrics(ch, aggregatedStats, metrics[0:])
+		close(ch)
+	}()
+
+	expected := []MetricResult{
+		{labels: labelMap{"schema": "test", "name": "table", "zoperation": "label1"}, value: 2},
+		{labels: labelMap{"schema": "test", "name": "table", "zoperation": "label2"}, value: 4},
+		{labels: labelMap{"schema": "test", "name": "table", "zoperation": "label3"}, value: 5},
+	}
+
+	convey.Convey("Metrics comparison", t, func() {
+		metricsReaded := 0
+		for elem := range ch {
+			metric := readMetric(elem)
+			convey.So(expected, convey.ShouldContain, metric)
+			metricsReaded++
+		}
+
+		convey.So(len(expected), convey.ShouldResemble, metricsReaded)
+	})
+}
+
+func rowProviderFunc(perfSchemaTableLockWaitsRows *sql.Rows) (TableStats, error) {
+	var (
+		tableSchema         string
+		tableName           string
+		rowsRead            uint64
+		rowsChanged         uint64
+		rowsChangedXIndexes uint64
+	)
+
+	err := perfSchemaTableLockWaitsRows.Scan(
+		&tableSchema,
+		&tableName,
+		&rowsRead,
+		&rowsChanged,
+		&rowsChangedXIndexes,
+	)
+	if err != nil {
+		return TableStats{}, err
+	}
+
+	tempStats := make(map[string]float64)
+	tempStats["rowsChanged"] = float64(rowsChanged)
+	tempStats["rowsChangedXIndexes"] = float64(rowsChangedXIndexes)
+	tempStats["rowsRead"] = float64(rowsRead)
+
+	tempLabels := make(map[string][]string)
+	tempLabels["rowsChanged"] = []string{"label1"}
+	tempLabels["rowsChangedXIndexes"] = []string{"label2"}
+	tempLabels["rowsRead"] = []string{"label3"}
+
+	return TableStats{tableSchema, tableName, []string{}, tempStats, tempLabels}, nil
+}
+
+func TestProcessRow(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+	aggregator := NewTableAggregator(*Regex, *Substitution)
+	aggregatedStats := make(map[string]TableStats)
+
+	columns := []string{"TABLE_SCHEMA", "TABLE_NAME", "ROWS_READ", "ROWS_CHANGED", "ROWS_CHANGED_X_INDEXES"}
+	rows := sqlmock.NewRows(columns).
+		AddRow("mysql", "proxies_priv", 99, 1, 0).
+		AddRow("mysql", "user", 1064, 2, 5)
+	mock.ExpectQuery(sanitizeQuery("select * from test.query")).WillReturnRows(rows)
+
+	mockRows, _ := db.Query("select * from test.query")
+
+	for mockRows.Next() {
+		aggregator.processRow(rowProviderFunc, mockRows, aggregatedStats)
+	}
+
+	expectedStats := map[string]TableStats{
+		"mysql.proxies_priv": TableStats{
+			"mysql",
+			"proxies_priv",
+			[]string{},
+			map[string]float64{
+				"rowsChanged":         float64(1),
+				"rowsChangedXIndexes": float64(0),
+				"rowsRead":            float64(99),
+			},
+			map[string][]string{
+				"rowsChanged":         []string{"label1"},
+				"rowsChangedXIndexes": []string{"label2"},
+				"rowsRead":            []string{"label3"},
+			},
+		},
+		"mysql.user": TableStats{
+			"mysql",
+			"user",
+			[]string{},
+			map[string]float64{
+				"rowsChanged":         float64(2),
+				"rowsChangedXIndexes": float64(5),
+				"rowsRead":            float64(1064),
+			},
+			map[string][]string{
+				"rowsChanged":         []string{"label1"},
+				"rowsChangedXIndexes": []string{"label2"},
+				"rowsRead":            []string{"label3"},
+			},
+		},
+	}
+
+	convey.Convey("Metrics comparison", t, func() {
+		metricsReaded := 0
+		for key, metric := range aggregatedStats {
+			expectedStat := expectedStats[key]
+			convey.So(expectedStat, convey.ShouldResemble, metric)
+			metricsReaded++
+		}
+
+		convey.So(len(expectedStats), convey.ShouldResemble, metricsReaded)
+	})
+}
+
+func TestProcessRowWithRegex(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+	aggregator := NewTableAggregator("(.*)\\d", "$1")
+	aggregatedStats := make(map[string]TableStats)
+
+	columns := []string{"TABLE_SCHEMA", "TABLE_NAME", "ROWS_READ", "ROWS_CHANGED", "ROWS_CHANGED_X_INDEXES"}
+	rows := sqlmock.NewRows(columns).
+		AddRow("mysql", "user2", 99, 1, 0).
+		AddRow("mysql", "user1", 1064, 2, 5)
+	mock.ExpectQuery(sanitizeQuery("select * from test.query")).WillReturnRows(rows)
+
+	mockRows, _ := db.Query("select * from test.query")
+
+	for mockRows.Next() {
+		aggregator.processRow(rowProviderFunc, mockRows, aggregatedStats)
+	}
+
+	expectedStats := map[string]TableStats{
+		"mysql.user": TableStats{
+			"mysql",
+			"user",
+			[]string{},
+			map[string]float64{
+				"rowsChanged":         float64(3),
+				"rowsChangedXIndexes": float64(5),
+				"rowsRead":            float64(1163),
+			},
+			map[string][]string{
+				"rowsChanged":         []string{"label1"},
+				"rowsChangedXIndexes": []string{"label2"},
+				"rowsRead":            []string{"label3"},
+			},
+		},
+	}
+
+	convey.Convey("Metrics comparison", t, func() {
+		metricsReaded := 0
+		for key, metric := range aggregatedStats {
+			expectedStat := expectedStats[key]
+			convey.So(expectedStat, convey.ShouldResemble, metric)
+			metricsReaded++
+		}
+
+		convey.So(len(expectedStats), convey.ShouldResemble, metricsReaded)
+	})
+}
